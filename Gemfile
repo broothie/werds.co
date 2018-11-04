ca78dd2c2135886c4ed6aaa55151fc415a92354a
Andrew Booth <booth@shipt.com>
2018-11-03 18:24:15 -0700

    add redis

--- Gemfile
+++ Gemfile
@@ -1,8 +1,10 @@
 ruby '2.5.1'
 source "https://rubygems.org"
 
-gem "sinatra", "~> 2.0"
-
 group :production do
   gem "puma", "~> 3.12"
 end
+
+gem "sinatra", "~> 2.0"
+
+gem "redis", "~> 4.0"
--- app.rb
+++ app.rb
@@ -1,19 +1,25 @@
 require 'sinatra'
-require 'base64'
+require 'redis'
+require 'securerandom'
+
+set :key_size, 8
+set :redis, Redis.new(url: ENV['REDIS_URL'])
 
 not_found { redirect to '/' }
 
 get '/' do
-  if text = params['t']
-    redirect to "/#{Base64.urlsafe_encode64(text)}"
+  if text = params['text']
+    key = SecureRandom.urlsafe_base64(settings.key_size)
+    settings.redis.set(key, text)
+    redirect to "/#{key}"
   else
     erb :index
   end
 end
 
-get '/:hash' do |hash|
+get '/:key' do |key|
   begin
-    @text = Base64.urlsafe_decode64(hash)
+    @text = settings.redis.get(key).gsub("\n", '<br/>')
   rescue
     redirect to '/'
   end
