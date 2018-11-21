7cdc227f8e00d4ea465fdf2e8e51c8ccc7adb6b1
Andrew Booth <booth@shipt.com>
2018-11-21 17:47:48 -0500

    use var instead of const

--- public/main.js
+++ public/main.js
@@ -1,14 +1,14 @@
 
 // https://github.com/30-seconds/30-seconds-of-code#copytoclipboard-
+// with some edits
 function copyToClipboard(str) {
-    const el = document.createElement('textarea');
+    var el = document.createElement('textarea');
     el.value = str;
     el.setAttribute('readonly', '');
     el.style.position = 'absolute';
     el.style.left = '-9999px';
     document.body.appendChild(el);
-    const selected =
-        document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+    var selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
     el.select();
     document.execCommand('copy');
     document.body.removeChild(el);
