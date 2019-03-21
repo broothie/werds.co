ca939a46ca05698d93f133d6b08f565b14d14e2c
Andrew Booth <booth@shipt.com>
2019-03-21 16:27:06 -0700

    use vulfpeck mono, use shift-enter

---
+++ public/js/index.js
@@ -0,0 +1,29 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var textarea = document.getElementById('werds');
+    var form = document.getElementById("form");
+
+    // Grows textarea vertically
+    function growTextArea() {
+        if (textarea.scrollHeight > textarea.clientHeight) {
+            textarea.style.height = textarea.scrollHeight + 'px';
+        }
+    }
+
+    growTextArea();
+
+    // Adds newlines on shift+enter
+    textarea.addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+
+            if (e.shiftKey) {
+                textarea.value = textarea.value + "\n";
+            } else {
+                form.submit();
+            }
+        }
+    });
+
+    textarea.addEventListener('keyup', growTextArea)
+});
